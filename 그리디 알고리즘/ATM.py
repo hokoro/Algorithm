78f2ef68a22272e1b382ba2941856f1bd2eb11b9
hokoro <hokoro123@gmail.com>
2022-01-05 19:15:09 +0900

    ATM (그리디)

---
+++ 그리디 알고리즘/ATM.py
@@ -0,0 +1,12 @@
+people = int(input())
+use_time = list(map(int, input().split()))
+accumulated_time = []
+answer = 0
+sum_time = 0
+use_time.sort()
+
+for time in use_time:
+    sum_time = sum_time + time
+    accumulated_time.append(sum_time)
+
+print(sum(accumulated_time))
