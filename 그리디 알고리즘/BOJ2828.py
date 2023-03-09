503979fe1ee4867d00a792c7d62c06b9cdeffffd
영성천 <hokoro123@gmail.com>
2023-03-09 20:29:25 +0900

    알고리즘 문제 풀이

--- 그리디 알고리즘/BOJ2828.py
+++ 그리디 알고리즘/BOJ2828.py
@@ -1,3 +1,26 @@
 import sys
 
 input = sys.stdin.readline
+
+n, m = map(int, input().split())
+
+j = int(input())
+
+spots = [int(input()) for _ in range(j)]
+
+x = 1
+answer = 0
+for spot in spots:
+    start = x
+    end = x + m -1
+    if start <= spot <= end:
+        continue
+    if spot < start:
+        answer += start - spot
+        x = spot
+        continue
+    if end < spot:
+        answer += spot - end
+        x = x + spot - end
+        continue
+print(answer)
